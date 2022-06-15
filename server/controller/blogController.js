00b6d1a14fe4f2181dbff698b2234c5fa7d95207
spkorn <s_meelarp@hotmail.com>
2022-06-15 10:36:43 +0700

    validate request data

--- server/controller/blogController.js
+++ server/controller/blogController.js
@@ -3,7 +3,18 @@ import slugify from "slugify";
 // บันทึกข้อมูล
 export const create = (req, res) => {
   const { title, content, author } = req.body;
-  const slug = slugify(req.body.slug);
+  const slug = slugify(title);
+
+  // validate / ตรวจสอบความถูกต้องของข้อมูล
+  switch (true) {
+    case !title:
+      return res.status(400).json({ error: "กรุณาป้อนชื่อบทความ" });
+      break;
+    case !content:
+      return res.status(400).json({ error: "กรุณาป้อนเนื้อหาบทความ" });
+      break;
+  }
+
   res.json({
     data: { title, content, author, slug },
   });
